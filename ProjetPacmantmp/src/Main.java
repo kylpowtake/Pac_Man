123ee8b7323c01c82a1530cb2973904bb1066f9e
Unknown <kylianviaux@hotmail.fr>
2018-10-25 14:34:12 +0200

    Je sais pas ce que j'ai fais

--- ProjetPacmantmp/src/Main.java
+++ ProjetPacmantmp/src/Main.java
@@ -2,26 +2,10 @@
 public class Main {
 	
 	public static void main(String[] args) {
-/*
-	  SimpleGame game = new SimpleGame();
-	  game.NbToursMax = 2;
-	  
-	  game.initializeGame();
-	  
-	  game.step();
-	  game.step();
-	  
-	  int i = game.getNbTours();
-	  System.out.println(i);
-	}
-	*/
 		
 		SimpleGame game = new SimpleGame();
 		ControleurGame controleur = new ControleurGame(game);
-		
-		game.initializeGame();
-		game.launch();
-		
+	
 		
 	
 		
