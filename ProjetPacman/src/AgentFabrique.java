a0d6421fbdb311d2c85f87d201e34eec54353d17
Unknown <kylianviaux@hotmail.fr>
2018-12-14 09:30:52 +0100

    Fabrique bien

--- ProjetPacman/src/AgentFabrique.java
+++ ProjetPacman/src/AgentFabrique.java
@@ -1,41 +1,27 @@
 
 public class AgentFabrique {
-	public Agent createAgent(boolean typeAgent,PositionAgent p,EnumComportement c){
-		
-		//agent pacman 
-		if(typeAgent == true){
-			ComportementFantomeFacile compo = new ComportementFantomeFacile();
-			return new Agent(typeAgent,p, compo);
-			
-			/*
-			switch(c){
-			case c:
-				return new Agent(typeAgent,p,c);
-				break;
-			case c:
-				return new Agent(typeAgent,p,c);
-				break;
-			default :
-				return new Agent(typeAgent,p,c);
-				break;	
-			 */
-		}
-		//agent fantome 
-		else{
-			return new Agent(typeAgent,p,c);
-			
-			/*
-			switch(c){
-			case c:
-				return new Agent(typeAgent,p,c);
-				break;
-			case c:
-				return new Agent(typeAgent,p,c);
-				break;
-			default :
-				return new Agent(typeAgent,p,c);
-				break;
-			*/	
+	public Agent createAgent(boolean typeAgent,PositionAgent p,EnumComportement c){		
+			switch (c){
+			case PACMAN_INITIAL :
+				return new Agent(typeAgent,p,new ComportementPacman());
+			case FANTOME_INITIAL :
+				return new Agent(typeAgent,p,new ComportementPacman());
+			case PACMAN_NORMAL :
+				return new Agent(typeAgent,p,new ComportementPacman());
+			case PACMAN_YOLO :
+				return new Agent(typeAgent,p,new ComportementPacman());
+			case PACMAN_NORMAL_ALGO :
+				return new Agent(typeAgent,p,new ComportementPacman());
+			case PACMAN_YOLO_ALGO :
+				return new Agent(typeAgent,p,new ComportementPacman());
+			case FANTOME_NORMAL :
+				return new Agent(typeAgent,p,new ComportementPacman());
+			case FANTOME_EFFRAYE :
+				return new Agent(typeAgent,p,new ComportementPacman());
+			case FANTOME_NORMAL_ALGO :
+				return new Agent(typeAgent,p,new ComportementPacman());
+			case FANTOME_EFFRAYE_ALGO :
+				return new Agent(typeAgent,p,new ComportementPacman());
 				
 		}
 		return null;
