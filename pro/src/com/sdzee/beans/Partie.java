7e577394b1c4951ecc44ebf1677fd06133409bcf
Unknown <i.marignale972@hotmail.fr>
2019-03-04 14:15:45 +0100

    affichage du pseudo de l'utilisateur

--- pro/src/com/sdzee/beans/Partie.java
+++ pro/src/com/sdzee/beans/Partie.java
@@ -4,6 +4,7 @@ import java.sql.Timestamp;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import org.hibernate.annotations.GenericGenerator;
 
@@ -29,6 +30,8 @@ public class Partie {
 		private long score;
 		//Le temps de fin de la partie.
 		private Timestamp date;
+		
+		private String pseudoUtilisateur;
 	
 		
 		//Indique à hibernate que cette valeur s'incrémente et correspond à la colomne increment du tableau(l'id).
@@ -50,6 +53,23 @@ public class Partie {
 			this.id = id;
 		}
 		
+		
+		//indique à hibernate la colomne associé à cette variable.
+		@Transient
+		/**
+		 * Permet d'obtenir le pseudo de l'utilisateur.
+		 * @return le pseudo de l'utilisateur.
+		 */
+	    public String getPseudoUtilisateur() {
+	        return pseudoUtilisateur;
+	    }
+		/**
+		 * Permet de changer le pseudo de l'utilisateur.
+		 * @param pseudo : nouveau pseudo de l'utilisateur.
+		 */
+	    public void setPseudoUtilisateur( String pseudoUtilisateur ) {
+	        this.pseudoUtilisateur = pseudoUtilisateur;
+	    }
 		
 		
 		//indique à hibernate la colomne associé à cette variable.
