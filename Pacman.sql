07c365267f734a8f89add8d70148b70252623304
Unknown <i.marignale972@hotmail.fr>
2019-03-11 15:38:35 +0100

    Ajout de la base de données

--- Pacman.sql
+++ Pacman.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le :  jeu. 14 fév. 2019 à 13:39
+-- Généré le :  lun. 11 mars 2019 à 15:35
 -- Version du serveur :  10.1.35-MariaDB
 -- Version de PHP :  7.2.9
 
@@ -30,9 +30,63 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `Partie` (
   `id` int(11) NOT NULL,
+  `idUtilisateur` int(11) NOT NULL,
   `score` int(11) NOT NULL,
+  `fantomeManges` int(11) NOT NULL,
+  `capsulesManges` int(11) NOT NULL,
+  `pacGommesManges` int(11) NOT NULL,
+  `mapEffectuees` int(11) NOT NULL,
+  `pasEffectues` int(11) NOT NULL,
   `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_bin;
+
+--
+-- Déchargement des données de la table `Partie`
+--
+
+INSERT INTO `Partie` (`id`, `idUtilisateur`, `score`, `fantomeManges`, `capsulesManges`, `pacGommesManges`, `mapEffectuees`, `pasEffectues`, `date`) VALUES
+(1, 1, 1, 0, 0, 0, 0, 0, '2019-02-26 13:12:30'),
+(2, 1, 245, 0, 0, 0, 0, 0, '2019-02-26 13:27:40'),
+(3, 1, 198, 0, 0, 0, 0, 0, '2019-02-26 13:29:42'),
+(4, 1, 48, 0, 0, 0, 0, 0, '2019-02-26 13:31:51'),
+(5, 1, 2, 0, 0, 0, 0, 0, '2019-02-26 13:34:19'),
+(6, 1, 86, 0, 0, 0, 0, 0, '2019-02-26 13:41:19'),
+(7, 1, 98, 0, 0, 0, 0, 0, '2019-02-26 13:42:57'),
+(8, 1, 253, 0, 0, 0, 0, 0, '2019-02-26 13:44:11'),
+(9, 1, 25, 0, 0, 0, 0, 0, '2019-02-26 13:47:30'),
+(10, 1, 1, 0, 0, 0, 0, 0, '2019-02-26 13:48:47'),
+(11, 1, 55, 0, 0, 0, 0, 0, '2019-02-26 13:51:09'),
+(12, 1, 59, 0, 0, 0, 0, 0, '2019-02-26 13:53:04'),
+(13, 1, 87, 0, 0, 0, 0, 0, '2019-02-26 13:55:43'),
+(14, 1, 782, 0, 0, 0, 0, 0, '2019-02-26 14:00:13'),
+(15, 1, 41, 0, 0, 0, 0, 0, '2019-02-26 14:04:24'),
+(16, 1, 0, 0, 0, 0, 0, 0, '2019-02-26 14:05:05'),
+(17, 1, 15, 0, 0, 0, 0, 0, '2019-02-26 14:11:56'),
+(18, 1, 1, 0, 0, 0, 0, 0, '2019-02-26 14:12:14'),
+(19, 1, 0, 0, 0, 0, 0, 0, '2019-02-26 14:12:27'),
+(20, 1, 180, 0, 0, 0, 0, 0, '2019-02-26 14:13:53'),
+(21, 1, 0, 0, 0, 0, 0, 0, '2019-02-26 14:51:18'),
+(22, 1, 0, 0, 0, 0, 0, 0, '2019-02-26 14:53:46'),
+(23, 1, 0, 0, 0, 0, 0, 0, '2019-02-26 14:54:24'),
+(24, 1, 0, 0, 0, 0, 0, 0, '2019-02-26 14:54:54'),
+(25, 1, 0, 0, 0, 0, 0, 0, '2019-02-26 14:55:06'),
+(26, 1, 0, 0, 0, 0, 0, 0, '2019-02-26 14:56:47'),
+(27, 1, 0, 0, 0, 0, 0, 0, '2019-02-26 14:56:55'),
+(28, 1, 0, 0, 0, 0, 0, 0, '2019-02-26 14:58:34'),
+(29, 1, 0, 0, 0, 0, 0, 0, '2019-02-26 15:01:45'),
+(30, 1, 0, 0, 0, 0, 0, 0, '2019-02-26 15:02:06'),
+(31, 1, 0, 0, 0, 0, 0, 0, '2019-02-26 15:09:43'),
+(32, 1, 0, 0, 0, 0, 0, 0, '2019-02-26 15:09:58'),
+(33, 1, 0, 0, 0, 0, 0, 0, '2019-03-06 14:50:12'),
+(34, 1, 0, 0, 0, 0, 0, 0, '2019-03-06 15:50:27'),
+(35, 1, 0, 0, 0, 0, 0, 0, '2019-03-06 15:51:24'),
+(36, 1, 0, 0, 0, 0, 0, 0, '2019-03-06 15:51:31'),
+(37, 3, 0, 0, 0, 0, 0, 0, '2019-03-06 15:52:53'),
+(38, 3, 0, 0, 0, 0, 0, 0, '2019-03-06 15:55:35'),
+(39, 3, 0, 0, 0, 0, 0, 0, '2019-03-06 15:55:48'),
+(40, 3, 0, 0, 0, 0, 0, 0, '2019-03-06 16:15:51'),
+(41, 1, 0, 0, 0, 0, 0, 0, '2019-03-06 16:20:43'),
+(42, 4, 1, 0, 0, 0, 0, 0, '2019-03-11 13:11:36');
 
 -- --------------------------------------------------------
 
@@ -43,9 +97,20 @@ CREATE TABLE `Partie` (
 CREATE TABLE `Utilisateur` (
   `id` int(11) NOT NULL,
   `pseudo` varchar(30) COLLATE utf32_bin NOT NULL,
-  `mot_de_passe` int(30) NOT NULL,
-  `date_inscription` datetime NOT NULL
+  `mot_de_passe` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `date_inscription` datetime NOT NULL,
+  `activite` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_bin;
+
+--
+-- Déchargement des données de la table `Utilisateur`
+--
+
+INSERT INTO `Utilisateur` (`id`, `pseudo`, `mot_de_passe`, `date_inscription`, `activite`) VALUES
+(1, 'test', 'PeaNlV2+Pifopdw7IlE5qfJCudpoWu0Ee/US3NVBA50hOoa8k5xtvg==', '2019-03-04 12:49:04', 0),
+(2, 'aaa', 'CIHCkXOmMZrfEEc+w6ITokSI53jOLZUiBgzTN7f6J0M0Ek00Q7enLg==', '2019-03-05 16:03:44', 1),
+(3, 'abc', 'PNgdZLeMXP8FT0XTxggBh0S0RvqHP5UTBRRn6q+mMtlCZ2Sr0Tklgg==', '2019-03-06 14:56:37', 1),
+(4, 'zzz', 'qc12mqsfxf35W6ICrLxMtcQxxhnMS+l5KRf+ohAbLXqlF6f4L5/LrQ==', '2019-03-11 13:09:49', 1);
 
 --
 -- Index pour les tables déchargées
@@ -55,7 +120,8 @@ CREATE TABLE `Utilisateur` (
 -- Index pour la table `Partie`
 --
 ALTER TABLE `Partie`
-  ADD KEY `id` (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_utilisateur` (`idUtilisateur`);
 
 --
 -- Index pour la table `Utilisateur`
@@ -68,10 +134,10 @@ ALTER TABLE `Utilisateur`
 --
 
 --
--- AUTO_INCREMENT pour la table `Utilisateur`
+-- AUTO_INCREMENT pour la table `Partie`
 --
-ALTER TABLE `Utilisateur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `Partie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- Contraintes pour les tables déchargées
@@ -81,7 +147,7 @@ ALTER TABLE `Utilisateur`
 -- Contraintes pour la table `Partie`
 --
 ALTER TABLE `Partie`
-  ADD CONSTRAINT `id` FOREIGN KEY (`id`) REFERENCES `Utilisateur` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `Partie_ibfk_1` FOREIGN KEY (`idUtilisateur`) REFERENCES `Utilisateur` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
