c46c1c58fb16aae68ae8dad8490f9475c1f64dc3
Unknown <i.marignale972@hotmail.fr>
2019-03-27 15:11:13 +0100

    Ajout des bâtiments dans la structre

--- Optimisation_Combinatoire/src/Main.java
+++ Optimisation_Combinatoire/src/Main.java
@@ -8,24 +8,27 @@ import javax.swing.JFrame;
 
 public class Main {
 	
+	static StructureUltime structureUltime = new StructureUltime();
+	
 	public static void Parser(String fileName){
 		String line = null;
     	try {
             FileReader fileReader =  new FileReader(fileName);
             BufferedReader bufferedReader =  new BufferedReader(fileReader);
-            int indiceLigne = 0;
-            
+            int indiceLigne = 0;   
             while((line = bufferedReader.readLine()) != null) {
             	String[] tableau = line.split(",");
             	switch(indiceLigne){
 	            	case 0:
-	            		System.out.println("taille de la structure : " + tableau[0] + " " + tableau[1]);
+	            		//System.out.println("taille de la structure : " + tableau[0] + " " + tableau[1]);
 	            		break;
 	            	case 1:
-	            		System.out.println("nombre de bâtiments : " + tableau[0]);
+	            		//System.out.println("nombre de bâtiments : " + tableau[0]);
 	            		break;
 	            	default:
-	            		System.out.println("taille du bâtiment : " + tableau[0] + " " + tableau[1]);
+	            		//System.out.println("taille du bâtiment : " + tableau[0] + " " + tableau[1]);
+	            		Double batiment = new Double(tableau[0],tableau[1]);
+	            		structureUltime.getBatiments().add(batiment);
 	            		break;
             	}
             	indiceLigne++;
@@ -44,7 +47,6 @@ public class Main {
 		FileDialog fd = new FileDialog(new JFrame());
 		fd.setVisible(true);
 		Parser(fd.getFiles()[0].getAbsolutePath());
-	}
 
 		System.out.println("Début.");
 		/*
