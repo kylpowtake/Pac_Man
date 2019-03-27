7a98ca7317470c3d8f80eb5999fb15a6942c6240
Unknown <i.marignale972@hotmail.fr>
2019-03-27 14:52:59 +0100

    Ajout du Parseur pour le projet d'optimisation

---
+++ Optimisation_Combinatoire/src/Main.java
@@ -0,0 +1,52 @@
+import java.awt.FileDialog;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
+public class Main {
+	
+	public static void Parser(String fileName){
+		String line = null;
+    	try {
+            FileReader fileReader =  new FileReader(fileName);
+            BufferedReader bufferedReader =  new BufferedReader(fileReader);
+            int indiceLigne = 0;
+            
+            while((line = bufferedReader.readLine()) != null) {
+            	String[] tableau = line.split(",");
+            	switch(indiceLigne){
+	            	case 0:
+	            		System.out.println("taille de la structure : " + tableau[0] + " " + tableau[1]);
+	            		break;
+	            	case 1:
+	            		System.out.println("nombre de bâtiments : " + tableau[0]);
+	            		break;
+	            	default:
+	            		System.out.println("taille du bâtiment : " + tableau[0] + " " + tableau[1]);
+	            		break;
+            	}
+            	indiceLigne++;
+            }   
+            bufferedReader.close();  
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println("Unable to open file '" + fileName + "'");                
+        }
+        catch(IOException ex) {
+        	ex.printStackTrace();
+        }
+	}
+	
+	public static void main(String[] args) {
+		FileDialog fd = new FileDialog(new JFrame());
+		fd.setVisible(true);
+		Parser(fd.getFiles()[0].getAbsolutePath());
+	}
+
+
+	
+
+}
