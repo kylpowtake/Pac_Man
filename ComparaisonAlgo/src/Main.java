14b1dfe946ab2e714eece709a7214f98c58f064d
Unknown <i.marignale972@hotmail.fr>
2019-04-02 08:53:58 +0200

    fin du truc sur Weka

--- ComparaisonAlgo/src/Main.java
+++ ComparaisonAlgo/src/Main.java
@@ -1,17 +1,16 @@
-import java.awt.FileDialog;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Random;
 import java.util.Set;
-
-import javax.swing.JFrame;
 import weka.classifiers.Evaluation;
 import weka.classifiers.trees.J48;
 import weka.core.Instances;
@@ -22,40 +21,45 @@ import weka.core.converters.ConverterUtils.DataSource;
 public class Main {
 	
 	public static String[] options;
-	public static String[] options2;
-	public static String chaineOption = "";
-	public static Map<Integer, Double> tableau  = new HashMap<Integer, Double>();
+	public static String[] optionsTemp;
+	public static String chaineOption = "";		
+	public static String fileNameA = "";		//nom du fichier avec chemin absolu
+	public static String fileName = "";			//nom du fichier 
+	public static Map<Integer, Double> tableau  = new HashMap<Integer, Double>();	//stocke les résultats obtenus après utilisation du classifieur
 	
 
 	public static void main(String[] args) {
-		
-		GestionParametres(args);
 		new Vue().setVisible(true);
-		/*
-		FileDialog fd = new FileDialog(new JFrame());
-		fd.setVisible(true);
+	}
+	
+	public static void executer(){
+		
 		
 		try {
-			DataSource source = new DataSource(fd.getFiles()[0].getAbsolutePath());
-			Instances data = source.getDataSet();
+			
+			DataSource source = new DataSource(fileNameA);	//chargement des données du fichier 
+			Instances data = source.getDataSet();			//creation du jeu de donnée 
 			if(data.classIndex() == -1){
 				data.setClassIndex(data.numAttributes() -1);
 			}
-			useClassifier(data);							//utilisation du classifier sur le jeu de données
-			writeFile(tableau,fd.getFiles()[0].getName());	//enregistrement des résultats dans un fichier csv
-			readFile(fd.getFiles()[0].getName());			//lecture du fichier er recupération du resultat le plus performant 
 			
-			//afichage du graphe 
-			JFrame p = new JFrame();
+			useClassifier(data);			//utilisation du classifier sur le jeu de données
+			writeFile(tableau,fileName);	//enregistrement des résultats dans un fichier csv
+			readFile(fileName);				//lecture du fichier er recupération du resultat le plus performant 
+			
+			//affichage du graphe 
 	    	LineChart ex = new LineChart(tableau);
-	    	p.add(ex);
-	    	p.setSize(700, 450);
-	    	p.setLocation(300,200);
-	        p.setVisible(true);
-			
+	    	Vue.graphPanel.removeAll();				
+	    	Vue.graphPanel.add(ex);
+	    	Vue.graphPanel.updateUI();
+	    	
+	    	//remise a 0 de la chaine d'option et du tableau de données 
+			chaineOption = "";
+			tableau.clear();
+	    	
 		} catch (Exception e) {
 			e.printStackTrace();
-		}*/
+		}
 	}
 
 	
@@ -71,7 +75,8 @@ public class Main {
 		Set<Entry<Integer, Double>> setHm = tableau.entrySet();
 	    Iterator<Entry<Integer, Double>> it = setHm.iterator();
 	    try{
-	        String file = "/home/etudiant/workspace/ComparaisonAlgo/" + filename + "_methodJ48.csv";
+	    	Path currentRelativePath = Paths.get("");
+	    	String file = currentRelativePath.toAbsolutePath().toString()+"/"+filename+"_methodJ48.csv";
 	        FileWriter fw = new FileWriter(file,true);
 	        fw.write("-M"+","+chaineOption+"\n");
 	        while(it.hasNext()){
@@ -98,12 +103,13 @@ public class Main {
         String nombreFeuilles = "";
         String paramètres = "";
         String paramètresOpti = "";
+        
         try {
             br = new BufferedReader(new FileReader(csvFile));
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(cvsSplitBy);
                 if(!values[0].equals("-M")){
-                	if(Double.parseDouble(values[1])<value){ 	
+                	if(Double.parseDouble(values[1])<value){ 
                     	nombreFeuilles = values[0];
                     	value = Double.parseDouble(values[1]);
                     	paramètresOpti = paramètres;
@@ -112,10 +118,15 @@ public class Main {
                 	paramètres = values[1];
                 }
             }
-            System.out.println("Valeur optimale : " + 
-            	   "\nparamètres utilisés : " + paramètresOpti +
- 				   "\nombre minimum d'elements pas feuille: -M " + nombreFeuilles + 
- 				   "\ntaux d'erreur : " + value);
+            
+          //affichage du meilleur résultat obtenu 
+	 	    printResults(tableau);
+            
+            Vue.resultLabel.append("\n\n------------------------------------\n\nMeilleur résultat\n" +
+            		"\nParamètres utilisés :\n" + paramètresOpti +
+            		"\nTaux d'erreur :\n" + value + "%" +
+ 				    "\nNombre d'éléments :\n" + nombreFeuilles);
+            
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -135,28 +146,30 @@ public class Main {
 	 * @param data (jeu de données utilisé)
 	 * @throws Exception
 	 * utilisation du la méthode de classification J48 sur les données passées en paramètres 
+	 * problème noté si il y a moins de 20 instances dans le jeu de données n'entre pas dans le for 
 	 */
 	protected static void useClassifier(Instances data) throws Exception {
 	    J48 base = new J48();
 	    options[options.length - 2] = "-M";
-	    options2 = new String[options.length];
+	    optionsTemp = new String[options.length];
 	    
 	    int nombreInstance = data.numInstances();
 	    int increment = nombreInstance/90;
-	    //System.out.println(nombreInstance);
-	    
+	    //System.out.println("nombre d'instances : " + nombreInstance);
+
 	    for(int i=2; i<nombreInstance*0.1; i+=increment){
 	    	options[options.length -1] = "" + i;
-	    	
+	    
 	    	//recopie du tableau pour ne pas perdre les données 
 	    	//une fois celles-ci consommées
 	    	for (int j =0;j<options.length;j++){
-	    		options2[j] = options[j];
+	    		optionsTemp[j] = options[j];
 	    	}
 	    	
 	    	//definit les options a utiliser sur notre classifieur 
 	    	//attention consomme les données 
-	    	base.setOptions(options2);
+	    	base.setOptions(optionsTemp);
+	    	
 	 	    Evaluation evaluation = new Evaluation(data);
 	 	    evaluation.crossValidateModel(base, data, 5, new Random(1));
 	 	    
@@ -167,164 +180,24 @@ public class Main {
 	
 	
 	/**
-	 * Méthode gérant les paramètres données par l'utilisateur pour le test actuel.
-	 * @param args
+	 * @param tableau
+	 * la méthode affiche le taux d'erreur le plus faible obtenu ainsi que 
+	 * le nombre d'element minimum par feuille qui y est associé
+	 * une fois la classification effectuée 
 	 */
-	public static void GestionParametres(String[] args){
-		//Le premier argument est le nom du fichier
-		//Le deuxième argument est le nom de la méthode à utiliser
-		//Les arguments suivants sont les paramètres de la méthode
-		//Il y a des paramètres contradictoires, ils ne peuvent être ensemble, un bolean est utilisé pour vérifié leur présence.
-		//Les derniers arguments sont les chemins vers les fichiers ou leurs noms.
-		//Le nombre de paramètres est  :  nombreArguments - 1(Nom fichier) - 1(Nom méthode) - 1~2(chemin, peut être deux chemins)
-		//Il y a donc un maximum de paramètres de NombreArguments - 3
-		//Et un minimum de paramètres de NombreArguments - 4
-		
-		//les options allant être utilisés pour ajoutés les paramètres à la méthode.
-		options = new String[args.length + 2];
-		//Le compteur pour savoir où placer l'argument actuel dans le tableau.
-		int compteur = 0;
-		
-		//Les options n'allant pas ensemble :
-		//  U et C
-		//  S et U
-		//  R/N/Q et U
-		//  R/N/Q et C
-		//  Si il y a R, N ou Q ==> il faut les deux autres, ils sont liés.
-		//  Il faut donc des booleans pour gérer ces situations
-		//  Si ils sont faux, ils ne sont pas présent, sinon ils sont vrai == présents
-		//  Il en faut au moins un de vrai (C , U , R/N/Q).
-		//  Le paramètre M est toujours présent, il est obligatoire.
-		boolean testU = false;
-		boolean testC = false;
-		boolean testR = false;
-		boolean testN = false;
-		boolean testQ = false;
-		boolean testS = false;
-		boolean testM = false;
-		//Il y a deux chemin vers des fichiers possibles,
-		// celui contenant l'ensemble de de données de l'entraînement.
-		// celui contenant l'ensemble de de données d'évaluation.
-		for(int i = 0; i < args.length; i++){
-			switch(args[i]){
-			case "-L" :
-				options[compteur] = args[i];
-				compteur++;
-				break;
-			case "-O" :
-				options[compteur] = args[i];
-				compteur++;
-				break;
-			case "-S" :
-				options[compteur] = args[i];
-				compteur++;
-				testS = true;
-				break;
-			case "-C" :
-				options[compteur] = args[i];
-				compteur++;
-				i++;
-				options[compteur] = args[i];
-				compteur++;
-				testC = true;
-				break;
-			case "-B" :
-				options[compteur] = args[i];
-				compteur++;
-				break;
-			case "-M" :
-				options[compteur] = args[i];
-				compteur++;
-				i++;
-				options[compteur] = args[i];
-				compteur++;
-				testM = true;
-				System.out.println("on y passe");
-				break;
-			case "-A" :
-				options[compteur] = args[i];
-				compteur++;
-				break;
-			case "-J" :
-				options[compteur] = args[i];
-				compteur++;
-				break;
-			case "-doNotMakeSplitPointActualValue" :
-				options[compteur] = args[i];
-				compteur++;
-				break;
-			case "-output-debug-info" :
-				options[compteur] = args[i];
-				compteur++;
-				break;
-			case "-do-not-check-capabilities" :
-				options[compteur] = args[i];
-				compteur++;
-				break;
-			case "-num-decimal-places" :
-				options[compteur] = args[i];
-				compteur++;
-				i++;
-				options[compteur] = args[i];
-				compteur++;
-				break;
-			case "-batch-size" :
-				options[compteur] = args[i];
-				compteur++;
-				i++;
-				options[compteur] = args[i];
-				compteur++;
-				break;
-			case "-N" :
-				options[compteur] = args[i];
-				compteur++;
-				i++;
-				options[compteur] = args[i];
-				compteur++;
-				testN = true;
-				break;
-			case "-Q" :
-				options[compteur] = args[i];
-				compteur++;
-				i++;
-				options[compteur] = args[i];
-				compteur++;
-				testQ = true;
-				break;
-			case "-R" :
-				options[compteur] = args[i];
-				compteur++;
-				testR = true;
-				break;
-			case "-U" :
-				options[compteur] = args[i];
-				compteur++;
-				testU = true;
-				System.out.println("on y passe");
-				break;
-			default :
-				System.out.println("on y passe" + args[i]);
-
-				//Seulement deux possibiltés autorisés,
-				//Le string est ce lui d'un des deux chemin possibles vers le fichier contenant l'ensemble de données(celui d'entraînement et celui d'évaluation)
-				break;
-			}
-		}
-		//On test la présence des paramètres obligatoires et de ceux contradictoires.
-		if((testU == false && testC == false && testQ == false) 
-				|| ((testQ != testR) || (testR != testN)) 
-				|| (testU == true && testC == true) 
-				|| (testU == true && testQ == true)
-				|| (testC == true && testQ == true)
-				|| (testU == true && testS == true)){
-			System.out.println("Erreur de paramètrage." + testM + testU);
-		} else {
-			System.out.println("Paramétrage réussi.");
-		}	
-		for(int i=0;i<options.length;i++){
-			if(options[i]!=null)
-				chaineOption += options[i]+" ";
-	    }
+	public static void printResults(Map<Integer, Double> tableau){
+		double tauxErreur = 100;
+		int nbFeuilles = 1000;
+		Set<Entry<Integer, Double>> setHm = tableau.entrySet();
+	    Iterator<Entry<Integer, Double>> it = setHm.iterator();
+	    while(it.hasNext()){
+	    	Entry<Integer, Double> e = it.next();
+	        if(e.getValue()<tauxErreur){
+	        	tauxErreur = e.getValue();
+	        	nbFeuilles = e.getKey();
+	        }
+	     }
+	    Vue.resultLabel.setText("\nNombre d'éléments  : \n" + nbFeuilles +
+				   				"\n\nTaux d'erreur : \n" + tauxErreur +"%");
 	}
-	
 }
--- ComparaisonAlgo/src/Vue.java
+++ ComparaisonAlgo/src/Vue.java
@@ -1,22 +1,34 @@
-import javax.swing.GroupLayout;
+import java.awt.TextArea;
+import java.awt.event.ComponentEvent;
+import java.awt.event.WindowEvent;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 
 
+@SuppressWarnings("serial")
+public class Vue extends JFrame{
+	
+	private 		JButton 	Executer;
+	@SuppressWarnings("rawtypes")
+	private		 	JComboBox	classifieur;
+	private 		JLabel 		jLabel1;
+    private 		JLabel 		jLabel2;
+    private 		JMenuBar 	jMenuBar1;
+    private 		JMenuItem 	menuch;
+    private 		JTextField 	textParams;
+    public static	JPanel 		jPanel1;
+    public static 	JPanel 		graphPanel;
+    public static 	TextArea 	resultLabel;
 
-
-public class Vue extends javax.swing.JFrame{
-	private JLabel labelParam;
-	private JLabel label;
-    private JMenuItem menuChoseFile;
-    private javax.swing.JMenuBar jMenuChoseFile;
-    private JPanel jPanelGlobal;
-    private JTextField texteParam;
-
+    
 	/**
 	 * Méthode pour construire la vue.
 	 */
@@ -27,58 +39,137 @@ public class Vue extends javax.swing.JFrame{
     /**
      * Méthode initialisant tous les comosants de la vue.
      */
-	private void InitComponents(){
-		//Initialisation basique des composants.
-		menuChoseFile = new javax.swing.JMenuItem();
-		jMenuChoseFile = new javax.swing.JMenuBar();
-        labelParam = new javax.swing.JLabel();
-        jPanelGlobal = new javax.swing.JPanel();
-        texteParam = new JTextField();
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private void InitComponents() {
+    	
+    	String[] ClassifieurString = {"J48"};
+    	
+        jPanel1 	= new JPanel();
+        Executer 	= new JButton();
+        graphPanel 	= new JPanel();
+        resultLabel = new TextArea();
+        textParams 	= new JTextField();
+        jLabel1		= new JLabel();
+        jLabel2 	= new JLabel();
+        jMenuBar1 	= new JMenuBar();
+        menuch		= new JMenuItem();
+        classifieur = new JComboBox(ClassifieurString);
+
+       //-----------------------------------------------------------------------------------------------------
         
+
+        jPanel1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 8, true));
         
+        graphPanel.setBackground(new java.awt.Color(204, 204, 204));
+        graphPanel.setOpaque(true);
         
-		//On donne l'affichage voulue.
-		jMenuChoseFile.setBorder(null);
-		jMenuChoseFile.add(menuChoseFile);
-        setJMenuBar(jMenuChoseFile);
-		
-        //On ajoute le texte au label devant l'entrée des paramètres voulues.
-        labelParam.setText("Paramètres à utiliser : ");
+        resultLabel.setBackground(new java.awt.Color(204, 204, 204));
+        resultLabel.setFont(new java.awt.Font("Tahoma", 3, 17));
+        resultLabel.setText("\n\n\n\n\n\n        En attente \n               de \n         traitement");
+        resultLabel.setEditable(false);
 
         
-		//On ajoute le texte à afficher sur le bouton
-		menuChoseFile.setText("Ouvrir un fichier");
-		//On ajoute le lu=istener sur le bouton.
-		menuChoseFile.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent event) {
-                menuchActionPerformed(event);
+        jLabel1.setText("Paramètres :");
+        jLabel2.setText("Classifieur :");
+        Executer.setEnabled(false);
+        
+        jMenuBar1.setBorder(null);
+        jMenuBar1.add(menuch);
+        setJMenuBar(jMenuBar1);
+        
+      //-----------------------------------------------------------------------------------------------------
+        
+        //ActionListeners
+        menuch.setText("Ouvrir un fichier");
+        menuch.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menuchActionPerformed(evt);
             }
         });
-		
-		
-		//On gère le layout des composants de la fenêtre.
-        javax.swing.GroupLayout jPanelLayout = new javax.swing.GroupLayout(jPanelGlobal);
-        jPanelGlobal.setLayout(jPanelLayout);
-        jPanelLayout.setHorizontalGroup(
-        		jPanelLayout.createSequentialGroup()
-        		.addComponent(labelParam)
-        		.addComponent(texteParam, javax.swing.GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE))
-        		;
-        jPanelLayout.setVerticalGroup(
-        		jPanelLayout.createParallelGroup()
-        		.addComponent(labelParam)
-        		.addComponent(texteParam, javax.swing.GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE))
-        		;
-		
-        GroupLayout layout = new GroupLayout(getContentPane());
+        
+        Executer.setText("Executer");
+        Executer.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+            	resultLabel.setText("\n\n\n\n          Traitement \n                 en \n           cours");
+                ExecuterActionPerformed(evt);
+            }
+        });
+        
+       
+      //-----------------------------------------------------------------------------------------------------
+        
+        //JFrame paramètres
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(800, 540));
+        setLocation(700,200);
+        setResizable(false);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
+            }
+			private void formWindowOpened(WindowEvent evt) {}
+        });
+        addComponentListener(new java.awt.event.ComponentAdapter() {
+            public void componentHidden(java.awt.event.ComponentEvent evt) {
+                formComponentHidden(evt);
+            }
+			private void formComponentHidden(ComponentEvent evt){}
+        });
+        
+        //-----------------------------------------------------------------------------------------------------
+        
+        //JPanel paramètres
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                    			.addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    			.addComponent(classifieur, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            	.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(textParams, javax.swing.GroupLayout.PREFERRED_SIZE, 400,javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(Executer, javax.swing.GroupLayout.PREFERRED_SIZE, 100,javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(graphPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 600, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(resultLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE)));
+        
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(30, 30, 30)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                	.addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                	.addComponent(classifieur, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(textParams, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(Executer,javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(30, 30, 30)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                		.addComponent(graphPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 440, javax.swing.GroupLayout.PREFERRED_SIZE)
+                		.addComponent(resultLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 440, javax.swing.GroupLayout.PREFERRED_SIZE)))
+        );
+
+      	//-----------------------------------------------------------------------------------------------------
+        
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-        		.addComponent(jPanelGlobal, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
-        layout.setVerticalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-        		.addComponent(jPanelGlobal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));       
-        //Rend la fenêtre visible et ajuste sa largeur et sa hauteur.
-		pack();
-	}
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }
+
 	
 	
 	
@@ -87,12 +178,41 @@ public class Vue extends javax.swing.JFrame{
      * FileChooser, permet de sélectionner un fichier dans le système.
      */
     private void menuchActionPerformed(java.awt.event.ActionEvent event) {
-        String fileName="";
         JFileChooser chooser = new JFileChooser();
         chooser.setApproveButtonText("choisir");
 		if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
-			fileName = chooser.getSelectedFile().getPath();
-			Main.readFile(fileName);
+			Main.fileNameA = chooser.getSelectedFile().getAbsolutePath();
+			Main.fileName = chooser.getSelectedFile().getName();
+			Executer.setEnabled(true);
+			
 	    }	
+    }
+    
+    
+    /**
+     * @param evt
+     * Onclick sur "Executer" 
+     * défini les paramètres et lance le classifieur  
+     */
+    private void ExecuterActionPerformed(java.awt.event.ActionEvent evt) {
+    	
+    	
+    	//récupération et enregistrement des options dans la variable prévue a cet effet
+    	String[] Options = textParams.getText().split(" ");
+    	Main.options = new String[Options.length + 2];
+    	for(int i=0; i< Options.length; i++){
+    		Main.options[i] = Options[i];
+    	}
+    	
+    	
+    	for(int i=0;i<Main.options.length;i++){
+			if(Main.options[i]!=null)
+				Main.chaineOption += Main.options[i]+" ";
+	    }
+    	
+    	//lance le classifier et génère le graphe
+    	resultLabel.setText("\n\n\n\n\n\n        Traitement \n               en \n             cours");
+    	Main.executer();
+
     }
 }
